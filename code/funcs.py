8445c128001d9ed5e865876c860457db272a1fbb
mac <mac@10-22-113-56.ed7.eduroam.fct.unl.pt>
2022-11-07 21:41:43 +0000

    end of lines windows type

--- code/funcs.py
+++ code/funcs.py
@@ -1,1 +1,9 @@
-#!/usr/bin/env python3# -*- coding: utf-8 -*-"""Created on Mon Nov  7 21:36:22 2022@author: mac"""
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Nov  7 21:36:22 2022
+
+@author: mac
+"""
+
+
