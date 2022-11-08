6b823db5e1f7710b6beab50faf0bfab55fa00632
VAKozyrev <vladimirkozyrev3@gmail.com>
2022-11-08 17:56:27 +0000

    validations + I command

--- code/smiles_strings_list_class.py
+++ code/smiles_strings_list_class.py
@@ -11,3 +11,6 @@ class SmilesStringsList:
 
     def __init__(self, smiles_strings_list):
         self.list_smiles = smiles_strings_list
+
+    def input(self, smiles):
+        self.list_smiles.append(smiles)
