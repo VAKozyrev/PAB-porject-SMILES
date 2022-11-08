c589e95d59bdc77f891764aecf6ace13ed28a935
VAKozyrev <vladimirkozyrev3@gmail.com>
2022-11-08 16:00:24 +0000

    _

--- class_SMILES.py
+++ class_SMILES.py
@@ -5,6 +5,6 @@ class Smiles:
         self.smiles = smiles
 
     def validate(self):
-        expression = '((Cl)|(Br)|[CNIFBOPcno])+(@{0,2}[0-9]{0,1}\({0,1}[\=#\/]{0,1}((Cl)|(Br)|[CNIFBOPcno])+\){0,1}[0-9]{0,1})*')
+        expression = '((Cl)|(Br)|[CNIFBOPcno])+(@{0,2}[0-9]{0,1}\({0,1}[\=#\/]{0,1}((Cl)|(Br)|[CNIFBOPcno])+\){0,1}[0-9]{0,1})*'
         match = re.match(expression, self.smiles)
         return match.group() == self.smiles
