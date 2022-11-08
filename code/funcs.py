47d3a0c84d759e8145cd4fef4e81741661f9ad6e
mac <mac@10-22-112-169.ed7.eduroam.fct.unl.pt>
2022-11-08 21:28:05 +0000

    +constants.py

--- code/funcs.py
+++ code/funcs.py
@@ -1,20 +1,33 @@
 
 from smiles_string_class import SmilesString
 from smiles_strings_list_class import SmilesStringsList
+import constants as c
 
-INPUT_NEW_SMILES = 'Enter new smiles '
-SMILES_INVALID   = 'SMILES invalid'
-SMILES_INSERTED  = 'SMILES Inserted'
+
+
 #def smiles_from_file_io():
 
 
 #def smiles_to_file_io():
 
 def input_new_io(list_smiles):
-    s = input(INPUT_NEW_SMILES)
+    s = input(c.INPUT_NEW_SMILES)
     smiles = SmilesString(s)
     if smiles.validate():
         list_smiles.input(smiles)
-        print(SMILES_INSERTED)
+        print(c.SMILES_INSERTED)
     else:
-        print(SMILES_INVALID)
+        print(c.SMILES_INVALID)
+
+
+#def obtain_molecular_formula():
+
+
+
+def open_file():
+    file_name = str(input(">"))
+    try:
+        with open("file_name", "r") as file_handle:
+            smiles_strings_list = file_handle.read()
+    except:
+        print(c.FAILED_READING)
