9a08f02225d021462a61d1691442624efb19bd20
VAKozyrev <vladimirkozyrev3@gmail.com>
2022-11-08 16:26:35 +0000

    validation function

--- code/smiles_string_class.py
+++ code/smiles_string_class.py
@@ -22,6 +22,6 @@ class SmilesString:
     '''
 
     def validate(self):
-        res = True
-
-        return res
+        expression = '((Cl)|(Br)|[CNIFBOPcno])+(@{0,2}[0-9]{0,1}\({0,1}[\=#\/]{0,1}((Cl)|(Br)|[CNIFBOPcno])+\){0,1}[0-9]{0,1})*'
+        match = re.match(expression, self.smiles)
+        return match.group() == self.smiles
