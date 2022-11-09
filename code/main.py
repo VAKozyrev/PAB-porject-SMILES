5f17e3bc6e545c11c30e19ff077db590ec46c98d
mac <mac@MacBook-Pro-mac.local>
2022-11-09 14:22:43 +0000

    main fix

--- code/main.py
+++ code/main.py
@@ -34,58 +34,59 @@ def main():
 
     if answer == c.NO:
         smiles_list = SmilesStringsList([])
-        print(c.HELP_MESSAGE)
+
+    print(c.HELP_MESSAGE)
+
+    command = f.read_command(c.PROMPT)
+
+    while command.upper() != c.QUIT:
+
+        #if command == c.COUNT_SUBSTRINGS:
+        #   answer = f.read_command(c.INPUT_SOURSE)
+        #    if answer == c.FILE:
+        #        file_name = f.read_command(c.PROMPT)
+        #        if f.open_file(file_name):
+        #            substrings_list = f.read_substrings_from_file(file_name)
+        #            if smiles_list.smiles_list == []:
+        #                print(c.LIST_IS_EMPTY)
+        #            else:
+        #                for smiles in smiles_list.smiles_list:
+        #                    print(smiles)
+        #        else:
+        #            print(c.FAILED_READING + str(file_name))
+
+
+
+        if command == c.MOLECULAR_FORMULA:
+            if smiles_list == []:
+                print(c.LIST_IS_EMPTY)
+            else:
+                f.obtain_molecular_formula(smiles_list)
+
+        #if command == DISSIMILARITY:
+
+        elif command == c.INPUT:
+            f.input_new_smiles(smiles_list)
+
+        #elif command ==
+
+        #if command == HELP:
+            #print(HELP_MESSAGE)
+
+        else:
+            print(c.INVALID_COMMAND)
 
         command = f.read_command(c.PROMPT)
 
-        while command.upper() != c.QUIT:
-
-            #if command == c.COUNT_SUBSTRINGS:
-            #   answer = f.read_command(c.INPUT_SOURSE)
-            #    if answer == c.FILE:
-            #        file_name = f.read_command(c.PROMPT)
-            #        if f.open_file(file_name):
-            #            substrings_list = f.read_substrings_from_file(file_name)
-            #            if smiles_list.smiles_list == []:
-            #                print(c.LIST_IS_EMPTY)
-            #            else:
-            #                for smiles in smiles_list.smiles_list:
-            #                    print(smiles)
-            #        else:
-            #            print(c.FAILED_READING + str(file_name))
-
-
-
-            if command == c.MOLECULAR_FORMULA:
-                if smiles_list == []:
-                    print(c.LIST_IS_EMPTY)
-                else:
-                    f.obtain_molecular_formula(smiles_list)
-
-            #if command == DISSIMILARITY:
-
-            elif command == c.INPUT:
-                f.input_new_smiles(smiles_list)
-
-            #elif command ==
-
-            #if command == HELP:
-                #print(HELP_MESSAGE)
-
-            else:
-                print(c.INVALID_COMMAND)
-
-            command = f.read_command(c.PROMPT)
-
-            if command == c.QUIT:
+        if command == c.QUIT:
+            answer = f.read_command(c.SAVE_SMILES)
+            while answer != c.YES and answer != c.NO:
+                print(c.INVALID_ANSWER)
                 answer = f.read_command(c.SAVE_SMILES)
-                while answer != c.YES and answer != c.NO:
-                    print(c.INVALID_ANSWER)
-                    answer = f.read_command(c.SAVE_SMILES)
 
-                if answer == c.YES:
-                    f.write_to_file(smiles_list)
+            if answer == c.YES:
+                f.write_to_file(smiles_list)
 
-                print(c.GOODBYE)
+    print(c.GOODBYE)
 
 main()
